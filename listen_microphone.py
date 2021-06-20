Fix overflow bug

listen_microphone.py
@@ -44,7 +44,7 @@ class ListenMusic():
     def start_listen(self, target_pitch):
         while True:
             try:
-                audiobuffer = self.stream.read(self.buffer_size)
+                audiobuffer = self.stream.read(self.buffer_size, exception_on_overflow = False)
                 signal = np.fromstring(audiobuffer, dtype=np.float32)
 
                 pitch = self.pitch_o(signal)[0]
@@ -59,6 +59,9 @@ class ListenMusic():
             except KeyboardInterrupt:
                 print("*** Ctrl+C pressed, exiting")
                 break
+            # except Exception as e:
+                # print(e)
+                # break
         self.close() 
         return False
 

main_form.py
@@ -4,11 +4,13 @@ import sys
 import os
 import cv2
 import pickle
+import threading
 import numpy as np
 from myui import Ui_Main_Form
 from mychange import create_page_sheet
 from listen_microphone import ListenMusic
 from utils import *
+
 
 FRET = 1 # temp
 
@@ -26,8 +28,7 @@ class WorkThread(QThread):
         self.flag = 1
 
         count = 0
-        while self.flag:
-            self.trigger.emit('test')
+        self.trigger.emit('start')
             # if self.flag == 1:
                 # count += 1
                 # print('in multithread\n')
@@ -38,6 +39,7 @@ class WorkThread(QThread):
 
     def stop(self):
         self.flag = 0
+        self.trigger.emit('stop')
 
 class Main_UI(QtWidgets.QMainWindow, Ui_Main_Form):
     def __init__(self, input_sheet_list):
@@ -87,8 +89,12 @@ class Main_UI(QtWidgets.QMainWindow, Ui_Main_Form):
         self.init_pixmap()
 
         ######################### multithread init
-        self.workthread = WorkThread()
-        self.workthread.trigger.connect(self.function_for_YX_to_use)
+        # self.workthread = WorkThread()
+        # self.workthread.trigger.connect(self.function_for_YX_to_use)
+        self.yx_flag = False
+        self.listenthread = threading.Thread(target = self.compare_listen_and_sheet)
+        # self.listenthread = WorkThread()
+        # self.listenthread.trigger.connect(self.compare_listen_and_sheet)
 
         self.listen = ListenMusic()
         #########################
@@ -119,14 +125,18 @@ class Main_UI(QtWidgets.QMainWindow, Ui_Main_Form):
 
     def listen_butten_clicked(self):
         
-        self.workthread.start()
+        # self.workthread.start()
+        self.listenthread.start()
+        # self.listenthread.run()
+        # self.workthread.run()
         # self.next_pushButton.setEnabled(False)
         # self.back_pushButton.setEnabled(False)
         self.stop_pushButton.setEnabled(True)
         print('listen button\n')
 
     def stop_button_clicked(self):
-        self.workthread.stop()
+        # self.workthread.stop()
+        # self.listenthread.stop()
         # self.next_pushButton.setEnabled(True)
         # self.back_pushButton.setEnabled(True)
         self.stop_pushButton.setEnabled(False)
@@ -212,22 +222,30 @@ class Main_UI(QtWidgets.QMainWindow, Ui_Main_Form):
         return information_list
 
     ## multithread trigger function
-    def function_for_YX_to_use(self, input_element):
+    # def function_for_YX_to_use(self, input_element):
+        # if input_element == 'start':
+            # self.yx_flag = True
+        # else:
+            # self.yx_flag = False
+        
+    def compare_listen_and_sheet(self):
         while self.now_note_number < self.max_note_number - 1:
             information_list = self.find_note_information(self.now_note_number)
-            string, pos = information_list[0][0], information_list[0][1]
-            now_midi = convert_pos2midi(string, pos, FRET)
+            pos,string = information_list[0][0], information_list[0][1]
+            now_midi = convert_pos2midi(string+1, pos, FRET)
+            print("play:", now_midi)
             if self.listen.start_listen(now_midi):
                 self.next_button_clicked()
                 continue
             else:
                 break
 
+
 if __name__ == "__main__":
     np.set_printoptions(threshold=sys.maxsize)
 
-    # input_sheet_list = ['resources/sample_guitar/sample_2.png', 'resources/sample_guitar/sample_3.png', 'resources/sample_guitar/sample_4.png', 'resources/sample_guitar/sample_5.png']
-    input_sheet_list = ['resources/sample_guitar/sample_2.png']
+    input_sheet_list = ['resources/sample_guitar/sample_2.png', 'resources/sample_guitar/sample_3.png', 'resources/sample_guitar/sample_4.png', 'resources/sample_guitar/sample_5.png']
+    # input_sheet_list = ['resources/sample_guitar/sample_2.png']
     
     app = QtWidgets.QApplication(sys.argv)
     window = Main_UI(input_sheet_list)